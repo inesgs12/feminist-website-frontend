20ca8152ccbec5afdf49949368ecbea287a1103e
Ines Guerrero <inesgs@gmail.com>
2019-08-10 09:43:58 -0600

    icon size from big to large

--- src/containers/AuthorId.js
+++ src/containers/AuthorId.js
@@ -53,7 +53,7 @@ class AuthorId extends React.Component {
                       this.props.isLiked ? "author-liked" : "author-not-liked"
                     }
                     name="like"
-                    size="big"
+                    size="large"
                   />
                   <br />
                 </div>
